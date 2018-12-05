84757d457df280d035bb40853781ab76c0a7495a
Junghoo Kim <jk4249@columbia.edu>
2018-12-05 00:09:12 -0400

    Fixing error and update

--- plotCam.py
+++ plotCam.py
@@ -2,6 +2,7 @@ from pylab import *
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.cbook import get_sample_data
 from matplotlib._png import read_png
+import numpy
 import math
 __author__ = "Junghoo Andy Kim"
 
@@ -23,12 +24,13 @@ def plot3D(tvec, imageNum, loc):
 	xQR, yQR = ogrid[0:qr.shape[0], 0:qr.shape[1]]
 	xQR = (xQR * qrLen / 330) - (qrLen / 2)
 	yQR = (yQR * qrLen / 330) - (qrLen / 2)
-	ax.plot_surface(xQR, yQR, 0, rstride=20, cstride=20, facecolors=qr)
+	ax.plot_surface(xQR, yQR, np.array(xQR)*0, rstride=20, cstride=20, facecolors=qr)
 
 
 	# Location of Camera and its direction
 	ax.scatter(x, y, z, marker='o', color='red')
-	ax.plot([x,loc[0]-x],[y,loc[1]-y],zs=[z,loc[2]-z], label='Camera Angle')
+	# print([x,loc[0]-x],[y,loc[1]-y],[z,loc[2]-z])
+	# ax.plot3D([x,loc[0]-x],[y,loc[1]-y],zs=z,zdir=loc[2]-z, label='Camera Angle')
 
 
 	# Axis range
